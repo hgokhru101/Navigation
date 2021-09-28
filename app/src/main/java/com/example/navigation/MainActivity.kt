33a2272ac0ada490bc4defa2a8808d4e3f0fdab4
hgokhru101 <harsh.gokhru@somaiya.edu>
2021-09-28 19:46:07 +0530

    NavigationJetPackAndNavigationDrawer

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var listener: NavController.OnDestinationChangedListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val actionBar=supportActionBar
+        navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
 
-        actionBar!!.setDisplayHomeAsUpEnabled(true)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
     }
 }
