a5df9f22ad1cb4613ee75ecc3ffba78dc4ca2f0d
hgokhru101 <harsh.gokhru@somaiya.edu>
2021-09-29 09:46:34 +0530

    Share Button And Menu In ActionBar

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.navigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -34,5 +37,31 @@ class MainActivity : AppCompatActivity() {
 
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+
+        // first parameter is the file for icon and second one is menu
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // We are using switch case because multiple icons can be kept
+        when (item.getItemId()) {
+            R.id.shareButton -> {
+                val sharingIntent = Intent(Intent.ACTION_SEND)
+                // type of the content to be shared
+                sharingIntent.type = "text/plain"
+                // Body of the content
+                val shareBody = "Your Body Here"
+                // subject of the content. you can share anything
+                val shareSubject = "Your Subject Here"
+                // passing body of the content
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody)
+                // passing subject of the content
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject)
+                startActivity(Intent.createChooser(sharingIntent, "Share using"))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
